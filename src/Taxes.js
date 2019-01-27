1962b52bc355b57d64b2bc44ba132f9f667b2b6c
Danny van Kooten <dannyvankooten@gmail.com>
2019-01-27 10:20:50 +0100

    add tax strategy vermogensbelasting 2019

--- src/Taxes.js
+++ src/Taxes.js
@@ -2,6 +2,27 @@ const Taxes = {
     'tax free': (capital, gains) => {
         return 0;
     },
+    
+    'vennootschapsbelasting 2021': (capital, gains) => {
+        if (gains < 0) {
+            return 0;
+        }
+        
+        const schijf_1 = Math.max(Math.min(200000, gains), 0)
+        const schijf_2 = Math.max(gains - 200000, 0)
+        return Math.max(Math.round(0.16 * schijf_1 + 0.21 * schijf_2), 0)
+    },
+    'vermogensbelasting 2019': (capital, gains) => {
+        capital = capital - 30360 ;
+        if (capital < 0) {
+            return 0;
+        }
+
+        const schijf_1 = Math.min(71650, capital)
+        const schijf_2 = Math.max(Math.min(989736, capital) - 71650, 0)
+        const schijf_3 = Math.max(capital - 989736, 0)
+        return Math.max(0, Math.round((0.01935 * schijf_1 + 0.04451 * schijf_2 + 0.0560 * schijf_3) * 0.30))    
+    },
 
     'vermogensbelasting 2018': (capital, gains) => {
         capital = capital - 30000;
@@ -12,8 +33,9 @@ const Taxes = {
         const schijf_1 = Math.min(70800, capital)
         const schijf_2 = Math.max(Math.min(978000, capital) - 70800, 0)
         const schijf_3 = Math.max(capital - 978000, 0)
-        return Math.max(0, Math.round((0.02017 * schijf_1 + 0.04326 * schijf_2 + 0.0538 * schijf_3) * 0.30))    },
-
+        return Math.max(0, Math.round((0.02017 * schijf_1 + 0.04326 * schijf_2 + 0.0538 * schijf_3) * 0.30))    
+    },
+    
     'vermogensbelasting 2017': (capital, gains) => {
         capital = capital - 25000;
         if (capital < 0) {
@@ -39,6 +61,7 @@ const Taxes = {
         const schijf_2 = Math.max(gains - 200000, 0)
         return Math.max(Math.round(0.19 * schijf_1 + 0.24 * schijf_2), 0)
     },
+
     'vennootschapsbelasting 2020': (capital, gains) => {
         if (gains < 0) {
             return 0;
@@ -48,15 +71,7 @@ const Taxes = {
         const schijf_2 = Math.max(gains - 200000, 0)
         return Math.max(Math.round(0.175 * schijf_1 + 0.225 * schijf_2), 0)
     },
-    'vennootschapsbelasting 2021': (capital, gains) => {
-        if (gains < 0) {
-            return 0;
-        }
-        
-        const schijf_1 = Math.max(Math.min(200000, gains), 0)
-        const schijf_2 = Math.max(gains - 200000, 0)
-        return Math.max(Math.round(0.16 * schijf_1 + 0.21 * schijf_2), 0)
-    },
+    
 }
 
 
--- src/Taxes.test.js
+++ src/Taxes.test.js
@@ -3,16 +3,25 @@ import Taxes from './Taxes.js';
 it('calculates taxes correctly', () => {
     const tests = [
         { 
+            // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/vermogen_en_aanmerkelijk_belang/vermogen/belasting_betalen_over_uw_vermogen/grondslag_sparen_en_beleggen/berekening-2019/voorbeeld-alleenstaande-met-spaargeld
+            strategy: 'vermogensbelasting 2019',
+            input: [130360, 0],
+            output: 794,
+        },
+        { 
+
             strategy: 'vermogensbelasting 2017',
             input: [125000, 0],
             output: 991,
         },
         { 
+            // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/vermogen_en_aanmerkelijk_belang/vermogen/belasting_betalen_over_uw_vermogen/grondslag_sparen_en_beleggen/berekenen_belasting_over_uw_inkomsten_uit_vermogen_vanaf_2017/voorbeeld-alleenstaande-met-125.000-euro-spaargeld
             strategy: 'vermogensbelasting 2017',
             input: [1250000, 0],
             output: 17108,
         },
         { 
+            // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/vermogen_en_aanmerkelijk_belang/vermogen/belasting_betalen_over_uw_vermogen/grondslag_sparen_en_beleggen/berekening-2018/Voorbeeld-alleenstaande-met-spaargeld
             strategy: 'vermogensbelasting 2018',
             input: [130000, 0],
             output: 807,
