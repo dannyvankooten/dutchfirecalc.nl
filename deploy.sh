644657ce8d9416270f6dc1c8834d4a360b8d8252
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-11-03 09:19:37 +0100

    exit deployment script on any error encountered

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
+set -e
+
 bin/fetch_latest_data.py
 
 ssh -t dutchfirecalc@s1.dvk.co "\
     cd ~/dutchfirecalc.nl; \
-    git pull; \
+    git pull -X theirs; \
     source ~/.cargo/env; \
     cargo build --release; \
     sudo systemctl restart dutchfirecalc; \
